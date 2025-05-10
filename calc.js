9131d9b5ffc00a38fd03129c7958635a8f010fc8
kemal-id <54977402+kemal-id@users.noreply.github.com>
2025-05-10 14:04:15 +0700

    Fix input operators before numbers

--- calc.js
+++ calc.js
@@ -37,23 +37,44 @@ function operatorButtonHandler(e) {
   if (e.target.nodeName == "UL" || displayed === "0") {
     return;
   }
+
+  let separated = separateDisplay();
+  console.log(separated);
+
+  if (separated[0] === regex) {
+    console.log(separated[0]);
+    return;
+  }
+
   if (e.target.innerText === "=") {
-    let separated = separateDisplay();
     if (Number.isNaN(separated[2])) {
       return;
     }
-    let result = operate(separated[0], separated[1], separated[2]);
-    updateDisplay(result, "=");
-    displayed = "";
+    if (separated[2]) {
+      let result = operate(separated[0], separated[1], separated[2]);
+      updateDisplay(result, "=");
+      displayed = "";
+    }
     return;
   }
-  if (displayed.toString().search(regex) !== -1) {
+
+  if(!separated[0] && !separated[1]) {
+    return;
+  }
+
+  if (displayed.search(regex) !== -1) {
     // to apply multiple operators before equals
+    console.log(displayed === "");
+    console.log(displayed === "0");
+    if (displayed === "" || displayed === "0") {
+      return;
+    }
     console.log(e.target.innerText);
-    let separated = separateDisplay();
-    let result = operate(separated[0], separated[1], separated[2]);
-    updateDisplay(result, "=");
-    updateDisplay(e.target.innerText);
+    if (separated[2]) {
+      let result = operate(separated[0], separated[1], separated[2]);
+      updateDisplay(result, "=");
+      updateDisplay(e.target.innerText);
+    }
     return;
   }
   updateDisplay(e.target.innerText);
@@ -84,7 +105,7 @@ function updateDisplay(word, equals = "") {
   console.log(word);
   if (equals === "=") {
     if (!Number.isSafeInteger(word)) {
-      Math.round(word * 100000) / 100000; //limiting decimal places to 5
+      word = Math.round(word * 100000) / 100000; //limiting decimal places to 5
       console.log(word);
     }
     displayed = Number(word);
